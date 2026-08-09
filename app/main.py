de41626ef610e75e68be5eb1db9b37b24498ae8d
Daiki <314608960+daiki-yama32@users.noreply.github.com>
2026-08-10 01:01:20 +0900

    Add initial Python application

---
+++ app/main.py
@@ -0,0 +1,8 @@
+def main():
+    projectname = "AI Technical Assistant"
+    print(f"Project Name: {projectname}")
+    print("Python enviroment is working!")
+
+
+if __name__ == "__main__":
+    main()
